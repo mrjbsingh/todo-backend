7fef6c94ded8b8f44d1761b3e92d85e7201c8ebb
mrjbsingh <jayendra.iiita@gmail.com>
2022-03-12 09:15:09 +0530

    disabled form login in spring security and enabled cors by overriding webSecurityConfigureAdapter

---
+++ src/main/java/com/jayjava/restwebservices/basicauth/SpringSecurityConfigurationBasicAuth.java
@@ -0,0 +1,24 @@
+package com.jayjava.restwebservices.basicauth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigurationBasicAuth extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> {
+            ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+        });
+        http.cors();
+        http.csrf().disable();
+        http.antMatcher("/**");
+        //http.formLogin();
+        http.httpBasic();
+    }
+}
